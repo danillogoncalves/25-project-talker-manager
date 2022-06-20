358d2f9379ed0ded1579ffe990527ce04ce07a4d
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-06-20 17:01:19 -0300

    :heavy_check_mark: feat: Requisito 1 finalizado.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +13,10 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async function(_req, res) {
+  const talker = await fs.readFile('./talker.json', 'utf-8');
+  res.status(200).json(JSON.parse(talker));
+});
 
 app.listen(PORT, () => {
   console.log('Online');
